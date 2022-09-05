991900cd6642dd917d3d22acc06d3137e3e1c1a1
Kaporos <theo@daron.be>
2022-09-05 17:35:40 +0200

    feat: ✨ Adding base stuff :D

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { Editor, Extension, mergeAttributes, NodeConfig, ReactNodeViewRenderer } from '@tiptap/react';
+import { ClassType, Component } from 'react';
+
+export function getBasicNodeConfiguration(
+  nodeName: string,
+  reactComponent: Component,
+  attributes: object | ClassType<any, any, any>,
+): Partial<NodeConfig> {
+  return {
+    title: nodeName,
+    addAttributes() {
+      return getAttributes(attributes);
+    },
+    parseHTML() {
+      return [
+        {
+          tag: nodeName,
+        },
+      ];
+    },
+    // @ts-ignore
+    renderHTML({ HTMLAttributes }) {
+      return [nodeName, mergeAttributes(HTMLAttributes)];
+    },
+    addNodeView() {
+      return ReactNodeViewRenderer(reactComponent);
+    },
+  };
+}
+
+export function getAttributes(sample: object | ClassType<any, any, any>) {
+  if (typeof sample === 'function') {
+    sample = new sample();
+  }
+  const keys: string[] = Object.keys(sample);
+  const attributes: { [key: string]: any } = {};
+  keys.forEach((key) => {
+    attributes[key] = { default: sample[key] };
+  });
+  return attributes;
+}
+
+export interface NodeProps<T> {
+  node: {
+    attrs: T;
+  };
+  editor: Editor;
+  extension: Extension;
+  deleteNode: () => void;
+  getPos: () => number;
+  selected: boolean;
+  updateAttributes: (newAttributes: T) => void;
+}
